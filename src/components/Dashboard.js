79f3a2c452f4189c63da90457850758495fd879b
Hrithik <slumdog4321@gmail.com>
2023-07-24 17:48:07 +0530

    dashBoard Ui part1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <h1 className="font-bold text-4xl text-green-600 pt-10 ml-10">
+        Welcome Back ,
+        <span className="text-white bg-green-300 font-semibold rounded-md px-3">User</span>
+      </h1>
+      <div className=" w-max flex mt-10 p-10 ">
+        <h2 className="text-2xl mr-20 border border-black p-1">Total Budget</h2>
+        <h3 className="text-2xl border border-black p-1">
+          0 /<input placeholder="total amount " className="w-max"></input> Rs.
+        </h3>
+      </div>
+      <div className="p-10 border-2 border-black w-max rounded-lg ml-10">
+        <h2 className="text-3xl font-semibold italic -mt-3 mb-2">Sub- Budget</h2>
+        <div>
+          <h3 className="text-2xl p-2">
+            Category :{" "}
+            <input placeholder="like: Food ,Rent" className="border border-black"></input>
+          </h3>
+          <h3 className="text-2xl p-2">
+            Budget : <input placeholder="0 Rs." className="border border-black ml-1"></input>
+          </h3>
+          <button className="border border-black bg-green-300 text-xl translate-x-80 p-2 rounded-lg">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
